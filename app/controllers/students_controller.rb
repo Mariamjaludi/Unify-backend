fdb1ebb73a5f853230837c9969e37223fb6a63fa
mariamjaludi <mariam.jaludi@gmail.com>
2019-06-11 21:46:10 +0100

    front-end mvp done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -27,27 +27,27 @@ class StudentsController < ApplicationController
     if @student.save
       payload= {student_id: student.id}
       token = issue_token(payload)
-      subject1 = Subject.find_by(name: student_params[:subject_grades][0][:subject])
-      subject2 = Subject.find_by(name: student_params[:subject_grades][1][:subject])
-      subject3 = Subject.find_by(name: student_params[:subject_grades][2][:subject])
+      # subject1 = Subject.find_by(name: student_params[:subject_grades][0][:subject])
+      # subject2 = Subject.find_by(name: student_params[:subject_grades][1][:subject])
+      # subject3 = Subject.find_by(name: student_params[:subject_grades][2][:subject])
 
-      student_subject1 = StudentSubject.create(
-        student: @student,
-        subject: subject1,
-        grade: student_params[:subject_grades][0][:grade]
-      )
-
-      student_subject2 = StudentSubject.create(
-        student: @student,
-        subject: subject2,
-        grade: student_params[:subject_grades][1][:grade]
-      )
-
-      student_subject3 = StudentSubject.create(
-        student: @student,
-        subject: subject3,
-        grade: student_params[:subject_grades][2][:grade]
-      )
+      # student_subject1 = StudentSubject.create(
+      #   student: @student,
+      #   subject: subject1,
+      #   grade: student_params[:subject_grades][0][:grade]
+      # )
+      #
+      # student_subject2 = StudentSubject.create(
+      #   student: @student,
+      #   subject: subject2,
+      #   grade: student_params[:subject_grades][1][:grade]
+      # )
+      #
+      # student_subject3 = StudentSubject.create(
+      #   student: @student,
+      #   subject: subject3,
+      #   grade: student_params[:subject_grades][2][:grade]
+      # )
 
       render json: { jwt: token }
     else
